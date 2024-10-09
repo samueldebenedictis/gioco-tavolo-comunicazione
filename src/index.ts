7ee77ba9fb8762123e6778fb59ba71ab6fd15793
Stefor07 <111366201+Sorecchione07435@users.noreply.github.com>
2024-10-09 15:38:03 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,18 @@
 import { Gioco } from "./gioco";
 
 // questa è la funzione principale
-function main() {
-	const gioco: Gioco = new Gioco(2, 40);
+function main() 
+{
+	// Variabili per l'avvio del gioco controllate dalla GUI
+
+	const nomiGiocatori = ["", "", "", ""];  // questo è l'array di stringhe che contiene il nome dei vari giocatori, il numero di stringhe è indeterminato
+	const giocatori: number = 4; // questa è una variabile che contiene il numero dei giocatori
+	const caselle: number = 40;
+
+	// il numero di stringhe nell'array nomiGiocatori deve corrispondere alla variabile numeroGiocatori nella classe Gioco
+
+	// Avvio del gioco con le informazioni fornite
+	const gioco: Gioco = new Gioco(giocatori, nomiGiocatori, caselle);
 	gioco.printInformazioniGioco();
 }
 
