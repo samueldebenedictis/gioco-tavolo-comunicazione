71d14f6304bbb6ff3daef1c92cabdfe33240a0d2
samueldebenedictis <samuel.debenedictis@yahoo.it>
2024-10-10 15:30:03 +0200

    test: fix tests

--- tests/gioco.test.ts
+++ tests/gioco.test.ts
@@ -1,27 +1,17 @@
-import { describe, expect, test } from '@jest/globals';
-import { Gioco } from '../src/gioco';
+import { describe, expect, test } from "@jest/globals";
+import { Gioco } from "../src/model/gioco";
 
-describe('Gioco', () => {
-    
-    
-    /*
-     test('La creazione di un gioco avviene con 2 giocatori e 40 caselle', () => {
-       
-       
-        const nomiGiocatori = ["Riccardo", "Elena"];
-        const gioco: Gioco = new Gioco(2, nomiGiocatori, 40);
-        expect(gioco.giocatori).toBe(2)
-        expect(gioco.tabellone).toHaveLength(40)
-    });
-    */
-   
+describe("Gioco", () => {
+  test("La creazione di un gioco avviene con 2 giocatori e 40 caselle", () => {
+    const nomiGiocatori = ["Riccardo", "Elena"];
+    const gioco: Gioco = new Gioco(nomiGiocatori, 40);
+    expect(gioco.numeroGiocatori).toBe(2);
+    expect(gioco.tabellone).toHaveLength(40);
+  });
 
-   /*   
-   test('La creazione di un gioco avviene con 3 giocatori e 60 caselle', () => {
-        const gioco: Gioco = new Gioco(3, 60);
-        expect(gioco.giocatori).toBe(3)
-        expect(gioco.tabellone).toHaveLength(60)
-    });
-   */
+  test("La creazione di un gioco avviene con 3 giocatori e 60 caselle", () => {
+    const gioco: Gioco = new Gioco(["Prova1","Prova2","Prova3"], 60);
+    expect(gioco.numeroGiocatori).toBe(3);
+    expect(gioco.tabellone).toHaveLength(60);
+  });
 });
-
