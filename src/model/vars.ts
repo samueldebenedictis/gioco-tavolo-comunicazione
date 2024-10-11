b41c17fc263713977106c8f30fa477ae40a0939a
samueldebenedictis <samuel.debenedictis@yahoo.it>
2024-10-11 16:02:47 +0200

    aggiunto file vars

---
+++ src/model/vars.ts
@@ -0,0 +1,7 @@
+// dichiarazioni delle varie macro globali per il gioco
+
+export const NUMERO_GIOCATORI_MASSIMO = 10;
+export const NUMERO_GIOCATORI_MINIMO = 2;
+export const NUMERO_CASELLE_PREDEFINITO = 40;
+export const separatore = "──────────────────────────────────────";
+export const NuovaLinea = "\n";
