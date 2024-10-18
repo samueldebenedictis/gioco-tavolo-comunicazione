bd63164c950f9c431b606a10b75c8966bae92f84
samueldebenedictis <samuel.debenedictis@yahoo.it>
2024-10-18 16:16:36 +0200

    aggiunti metodi lettura riga di comando

---
+++ src/utils/read.ts
@@ -0,0 +1,26 @@
+import { createInterface } from "node:readline";
+
+async function consoleQuestion(domanda: string) {
+	const rl = createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	const risposta = await new Promise((resolve) => {
+		rl.question(domanda, resolve);
+	});
+
+	rl.close();
+	return risposta as string;
+}
+
+export async function consoleInt(testo: string) {
+	const risposta = await consoleQuestion(testo);
+	const intero = Number.parseInt(risposta);
+	return intero;
+}
+
+export async function consoleString(domanda: string) {
+	const risposta = await consoleQuestion(domanda);
+	return risposta;
+}
